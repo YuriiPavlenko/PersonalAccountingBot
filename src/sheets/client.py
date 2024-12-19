b2e6957089a9629a9d0ccc53af4877ed090a0778
Yurii Pavlenko <65398074+YuriiPavlenko@users.noreply.github.com>
2024-12-19 04:10:27 +0000

    fixed imports

--- src/sheets/client.py
+++ src/sheets/client.py
@@ -1,11 +1,15 @@
+import json
+import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from src.config import GOOGLE_CREDENTIALS
 
 class SheetsClient:
     def __init__(self, spreadsheet_id):
         self.spreadsheet_id = spreadsheet_id
-        credentials = service_account.Credentials.from_service_account_file(
-            'credentials.json',
+        credentials_info = json.loads(GOOGLE_CREDENTIALS)
+        credentials = service_account.Credentials.from_service_account_info(
+            credentials_info,
             scopes=['https://www.googleapis.com/auth/spreadsheets']
         )
         self.service = build('sheets', 'v4', credentials=credentials)
