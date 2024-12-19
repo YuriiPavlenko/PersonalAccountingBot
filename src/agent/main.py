939fa79ed0498310b9324f9ecccc317be277cf1a
Yurii Pavlenko <65398074+YuriiPavlenko@users.noreply.github.com>
2024-12-19 04:39:24 +0000

    fixed imports

--- src/agent/main.py
+++ src/agent/main.py
@@ -1,4 +1,4 @@
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.agents import AgentExecutor, create_openai_functions_agent
 from langchain.prompts import ChatPromptTemplate
 
